e4ebea87c20a36ddd1a850ac083c9e8790bf54c3
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-07-17 14:10:58 +0900

    Update 0917_Reverse_Only_Letters.py

--- leetcode/0917_Reverse_Only_Letters.py
+++ leetcode/0917_Reverse_Only_Letters.py
@@ -1,11 +1,15 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        s = [(idx, i, i.isalpha()) for idx, i in enumerate(s)]
-        alphas = [i[1] for i in s if i[2]]
-        non_alphas = [i for i in s if not i[2]]
-        reversed_alphas = alphas[::-1]
-        
-        for i in non_alphas:
-            reversed_alphas.insert(i[0], i[1])
-        res = ''.join(reversed_alphas)
-        return res
+        s, i, j = list(s), 0, len(s) - 1
+
+        while i < j:
+            if not s[i].isalpha():
+                i += 1
+            elif not s[j].isalpha():
+                j -= 1
+            else:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+                
+        return ''.join(s)
