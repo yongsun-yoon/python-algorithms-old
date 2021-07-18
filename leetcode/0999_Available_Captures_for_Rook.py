82a293b87e2fcb48fd1d42e5e39d475926b67086
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-07-18 09:27:27 +0900

    Update 0999_Available_Captures_for_Rook.py

--- leetcode/0999_Available_Captures_for_Rook.py
+++ leetcode/0999_Available_Captures_for_Rook.py
@@ -2,12 +2,13 @@ from typing import List
 
 class Solution:
     def search(self, rr, rc, dr, dc, board):
-        while 1 <= rr < 7 and 1 <= rc < 7:
+        while 0 <= rr+dr <= 7 and 0 <= rc+dc <= 7:
             rr += dr
             rc += dc
+            
             if board[rr][rc] == 'p':
                 return 1
-            elif board[rr][rc] != '.':
+            elif board[rr][rc] == 'B':
                 return 0
         return 0
     
@@ -16,6 +17,5 @@ class Solution:
             for j in range(8):
                 if board[i][j] == 'R':
                     rr, rc = i, j
-        
         
         return self.search(rr, rc, 1, 0, board) + self.search(rr, rc, -1, 0, board) + self.search(rr, rc, 0, 1, board) + self.search(rr, rc, 0, -1, board)
