26e199c19dbda4f5237209a08a58aa84a5ac5cd3
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-07-19 07:31:14 +0900

    Update 1403_Minimum_Subsequence_in_Non-Increasing_Order.py

--- leetcode/1403_Minimum_Subsequence_in_Non-Increasing_Order.py
+++ leetcode/1403_Minimum_Subsequence_in_Non-Increasing_Order.py
@@ -3,11 +3,14 @@ from typing import List
 class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
         nums = sorted(nums)
-        total_sum = sum(nums)
+        remain_sum = sum(nums)
         
-        res = []
-        while sum(res) <= sum(nums):
+        current = []
+        current_sum = 0
+        while current_sum <= remain_sum:
             n = nums.pop()
-            res.append(n)
+            current.append(n)
+            current_sum += n
+            remain_sum -= n
         
-        return res
+        return current
