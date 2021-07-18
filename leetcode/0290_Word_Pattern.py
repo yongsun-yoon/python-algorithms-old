3d75bc0d76de707ffab0dd7b937cde9deada4418
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-07-19 07:52:30 +0900

    Update 0290_Word_Pattern.py

--- leetcode/0290_Word_Pattern.py
+++ leetcode/0290_Word_Pattern.py
@@ -1,16 +1,8 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
-        p2w, w2p = {}, {}
-        
-        if len(pattern) != len(s):
+        if len(s) != len(pattern): 
             return False
-        
-        for p, w in zip(pattern, s):
-            pw = p2w.setdefault(p, w)
-            wp = w2p.setdefault(w, p)
-            
-            if pw != w or wp != p:
-                return False
-        
-        return True
+
+        mapping = set(zip(pattern, s))
+        return len(set(s)) == len(set(pattern)) == len(mapping)
